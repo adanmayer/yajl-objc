c65f1e67341587c4002109ec60aecc3271e39742
Alexander Danmayer <A.Danmayer@innologix.at>
2014-10-23 10:09:44 +0200

    replace yajl-1.0.11 with yajl-2.1.1

--- yajl-2.1.1/api/yajl_version.h.cmake
+++ yajl-2.1.1/api/yajl_version.h.cmake
@@ -1,11 +1,11 @@
 #ifndef YAJL_VERSION_H_
 #define YAJL_VERSION_H_
 
-#include "yajl_common.h"
+#include <yajl/yajl_common.h>
 
-#define YAJL_MAJOR 1
-#define YAJL_MINOR 0
-#define YAJL_MICRO 11
+#define YAJL_MAJOR ${YAJL_MAJOR}
+#define YAJL_MINOR ${YAJL_MINOR}
+#define YAJL_MICRO ${YAJL_MICRO}
 
 #define YAJL_VERSION ((YAJL_MAJOR * 10000) + (YAJL_MINOR * 100) + YAJL_MICRO)
 
